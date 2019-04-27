84e8d01c88d1b6c14af82b188087dcaf9525b82b
Matthew <mjjply@Matthews-Air.attlocal.net>
2019-04-27 13:45:38 -0700

    added styles/incomplete

--- memegem/src/components/Header.js
+++ memegem/src/components/Header.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import './component-style.css'
 
 class Header extends Component {
-    constructor()   {
-        super()
-
-    }
     
     render()    {
         return  (
